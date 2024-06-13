a99d314a5a7ef544390214d8ed644ccc5d5bca4a
Tuan Chau <tuanqchau2810@gmail.com>
2024-06-13 16:21:44 -0400

    adjusted map viewport

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -6,18 +6,17 @@ import '../Styles/Map.css'
 
 
 const Map = () => {
-    const center = [10.772347575083762, 106.69750624727703]
+    const center = [15.877993203699853, 108.32814843938998]
     return (
         <MapContainer
         center={center}
-        zoom={5}
-        style={{width:'100vw',height:'100vh'}}>
-        
+        zoom={6}
+        style={{width:'50vw',height:'100vh'}}
+        >
             <TileLayer
             url='https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=bCJm1FRgtc2tZsmRppy7'
             attribution={'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'}
             >
-
             </TileLayer>
         </MapContainer>
 
