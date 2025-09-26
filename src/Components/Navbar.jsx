ab747da7bd6ea180c34f3b841f058ed1b1dbdbdb
tuanqchau <tuanqchau2810@gmail.com>
2025-09-25 21:18:49 -0400

    new design + photos

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,25 @@
 import {Link} from 'react-router-dom'
 import '../Styles/Navbar.css'
-import React, { useState, useContext } from 'react';
-//import { CursorContext } from '../context/CursorContext';
-const Navbar = () => {
-    //const {mouseEnterHandler, mouseLeaveHandler} = useContext(CursorContext);
+import React from 'react';
 
+const Navbar = () => {
     return (
         <div className='navbar-container'>
-            { <Link to={'/home'} className="nav-link" > 
-            Tuan Chau
-            </Link> }
-            <Link to={'/photos'} className="nav-link"> 
-            Gallery
+            <Link to={'/home'} className="navbar-brand"> 
+                Tuan Chau
             </Link>
-            <Link to={'/map'} className='nav-link'>
-            Map
-            </Link>
-            <Link to={'/about'} className="nav-link"> 
-            About
-            </Link>
-
-            
+            <div className="nav-links">
+                <Link to={'/photos'} className="nav-link"> 
+                    Gallery
+                </Link>
+                {/* <Link to={'/map'} className='nav-link'>
+                    Map
+                </Link> */}
+                <Link to={'/about'} className="nav-link"> 
+                    About
+                </Link>
+            </div>
         </div>
-        
     );
 }
 
