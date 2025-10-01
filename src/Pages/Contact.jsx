4666228f86cd3e0f6d2383419d0d7bc5d6c7b1c3
tuanqchau <tuanqchau2810@gmail.com>
2025-09-30 20:41:36 -0400

    contact page styling and configured with emailjs

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 import emailjs from "emailjs-com";
+import "../Styles/Contact.css";
+import Button from "../Components/Button";
 
 export default function Contact() {
   const form = useRef();
@@ -9,14 +11,14 @@ export default function Contact() {
 
     emailjs
       .sendForm(
-        "service_zzt4i3v",   // from EmailJS
-        "template_7p2de79",  // from EmailJS
+        "service_zzt4i3v",   
+        "template_7p2de79",  
         form.current,
-        "fTCW55-yPJzmmz42_"    // from EmailJS
+        "fTCW55-yPJzmmz42_" 
       )
       .then(
         () => {
-          alert("Message sent!");
+          //alert("Message sent!");
           form.current.reset();
         },
         (error) => {
@@ -26,15 +28,35 @@ export default function Contact() {
   };
 
   return (
-    <div style={{ maxWidth: "400px", margin: "auto" }}>
-      <h2>Chat With Me</h2>
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Email:</label>
-        <input type="email" name="user_email" required />
-        <label>Message:</label>
-        <textarea name="message" required />
-        <button type="submit">Send</button>
+    <div className="contact-container">
+      <h2 className="contact-title">Contact Me</h2>
+
+      <form ref={form} onSubmit={sendEmail} className="contact-form">
+        <div className="form-group">
+          {/* <label htmlFor="name">Name</label> */}
+          <input id="name" type="text" name="name" required placeholder="Name"/>
+        </div>
+
+        <div className="form-group">
+          {/* <label htmlFor="email">Email</label> */}
+          <input id="email" type="email" name="email" required placeholder="Email"/>
+        </div>
+
+        <div className="form-group">
+          {/* <label htmlFor="subject">Subject</label> */}
+          <input id="subject" type="text" name="title" required placeholder="Subject"/>
+        </div>
+
+        <div className="form-group">
+          {/* <label htmlFor="message">Message</label> */}
+          <textarea id="message" name="message" required placeholder="Type your message.."/>
+        </div>
+
+        {/* <button type="submit" className="contact-btn">Send</button> */}
+        <Button type="submit" className="contact-btn" text="Send"/>
       </form>
+
+      
     </div>
   );
 }
